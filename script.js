10d162644c63576acc5537782af90a01ae0d6cf6
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-04-26 18:24:39 +0530

    Add repeat feature

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ function playPause () {
         ctrlIcon.classList.remove("fa-pause");
         ctrlIcon.classList.add("fa-play");
     }
+    else if (ctrlIcon.classList.contains("fa-repeat")) {
+        song.currentTime = 0;
+        song.play();
+        ctrlIcon.classList.remove("fa-repeat");
+        ctrlIcon.classList.add("fa-pause");
+    }
     else {
         song.play();
         ctrlIcon.classList.remove("fa-play");
@@ -30,7 +36,7 @@ if (song.play()) {
         if ((song.duration - song.currentTime) <= 0.5) {
             song.pause();
             ctrlIcon.classList.remove("fa-pause");
-            ctrlIcon.classList.add("fa-play");
+            ctrlIcon.classList.add("fa-repeat");
         }
     }, 1000);
 }
